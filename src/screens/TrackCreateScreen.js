ce42b014844e86f55f0a63e0d5d4d9d4604de630
Cory Bass <cory.bass.07@gmail.com>
2021-04-19 17:37:34 -0500

    extracted location logic to reusable hook

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,48 +1,20 @@
 import "../_mockLocations";
-import React, { useEffect, useState, useContext } from "react";
+import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-navigation";
-import {
-    requestForegroundPermissionsAsync,
-    watchPositionAsync,
-    Accuracy,
-} from "expo-location";
 
 // context
 import { Context as LocationContext } from "../context/LocationContext";
 
 // local files
+import useLocation from "../hooks/useLocation";
 import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
     const { addLocation } = useContext(LocationContext);
-    const [err, setErr] = useState(null);
 
-    const startWatching = async () => {
-        try {
-            const { granted } = await requestForegroundPermissionsAsync();
-            await watchPositionAsync(
-                {
-                    accuracy: Accuracy.BestForNavigation,
-                    timeInterval: 1000,
-                    distanceInterval: 10,
-                },
-                (location) => {
-                    addLocation(location);
-                }
-            );
-            if (!granted) {
-                throw new Error("Location permission not granted");
-            }
-        } catch (e) {
-            setErr(e);
-        }
-    };
-
-    useEffect(() => {
-        startWatching();
-    }, []);
+    const [err] = useLocation(addLocation);
 
     return (
         <SafeAreaView forceInset={{ top: "always" }} style={{ flex: 1 }}>
