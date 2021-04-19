e9a69ec53ec3f96d227fd13523f5da00f9224686
Cory Bass <cory.bass.07@gmail.com>
2021-04-19 14:46:54 -0500

    added ability to log out and initialized page for creating a new track

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,8 +1,24 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-navigation";
+import { Button } from "react-native-elements";
+
+// context
+import { Context as AuthContext } from "../context/AuthContext";
+
+// local files
+import Spacer from "../components/Spacer";
 
 const AccountScreen = () => {
-    return <Text style={{ fontSize: 48 }}>AccountScreen</Text>;
+    const { signout } = useContext(AuthContext);
+    return (
+        <SafeAreaView forceInset={{ top: "always" }}>
+            <Text style={{ fontSize: 48 }}>AccountScreen</Text>
+            <Spacer>
+                <Button title="Sign Out" onPress={signout} />
+            </Spacer>
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({});
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { StyleSheet } from "react-native";
+import { Text } from "react-native-elements";
+import { SafeAreaView } from "react-navigation";
+
+// local files
+import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
-    return <Text style={{ fontSize: 48 }}>TrackCreateScreen</Text>;
+    return (
+        <SafeAreaView forceInset={{ top: "always" }}>
+            <Text h3>Create A Track</Text>
+            <Map />
+        </SafeAreaView>
+    );
 };
 
 const styles = StyleSheet.create({});
