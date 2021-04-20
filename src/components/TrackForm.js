d19b98feb450e6a6731fe41f9524f816c8097e91
Cory Bass <cory.bass.07@gmail.com>
2021-04-20 13:19:34 -0500

    working on bug where location array is not updating due to useEffect not running the second time

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Input, Button } from "react-native-elements";
+
+// context
+import { Context as LocationContext } from "../context/LocationContext";
 
 // local files
 import Spacer from "./Spacer";
 
 const TrackForm = () => {
+    const {
+        state: { name, recording, locations },
+        startRecording,
+        stopRecording,
+        changeName,
+    } = useContext(LocationContext);
+
+    console.log(locations.length);
+
     return (
         <>
             <Spacer>
-                <Input placeholder="Enter Name" />
+                <Input
+                    value={name}
+                    placeholder="Enter Name"
+                    onChangeText={changeName}
+                />
             </Spacer>
-            <Button title="Start Recording" />
+            {recording ? (
+                <Button title="Stop" onPress={stopRecording} />
+            ) : (
+                <Button title="Start Recording" onPress={startRecording} />
+            )}
         </>
     );
 };
