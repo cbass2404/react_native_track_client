d52f6ac06a03e2a7b472f576f14e034d831ec9fa
Cory Bass <cory.bass.07@gmail.com>
2021-04-20 12:22:09 -0500

    added form for track creation screen

---
+++ src/components/TrackForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Input, Button } from "react-native-elements";
+
+// local files
+import Spacer from "./Spacer";
+
+const TrackForm = () => {
+    return (
+        <>
+            <Spacer>
+                <Input placeholder="Enter Name" />
+            </Spacer>
+            <Button title="Start Recording" />
+        </>
+    );
+};
+
+export default TrackForm;
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -2,7 +2,7 @@ import "../_mockLocations";
 import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
-import { SafeAreaView } from "react-navigation";
+import { SafeAreaView, withNavigationFocus } from "react-navigation";
 
 // context
 import { Context as LocationContext } from "../context/LocationContext";
@@ -10,21 +10,22 @@ import { Context as LocationContext } from "../context/LocationContext";
 // local files
 import useLocation from "../hooks/useLocation";
 import Map from "../components/Map";
+import TrackForm from "../components/TrackForm";
 
-const TrackCreateScreen = () => {
+const TrackCreateScreen = ({ isFocused }) => {
     const { addLocation } = useContext(LocationContext);
-
-    const [err] = useLocation(addLocation);
+    const [err] = useLocation(isFocused, addLocation);
 
     return (
         <SafeAreaView forceInset={{ top: "always" }} style={{ flex: 1 }}>
             <Text h3>Create A Track</Text>
             <Map />
             {err ? <Text>Please enable location services</Text> : null}
+            <TrackForm />
         </SafeAreaView>
     );
 };
 
 const styles = StyleSheet.create({});
 
-export default TrackCreateScreen;
+export default withNavigationFocus(TrackCreateScreen);
