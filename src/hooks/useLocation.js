fb6075b6a2f653bf339dafa9154f0370445979ed
Cory Bass <cory.bass.07@gmail.com>
2021-04-21 18:27:39 -0500

    added ability to keep saving track when navigating away from creation screen

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -6,37 +6,36 @@ import {
 } from "expo-location";
 
 export default (shouldTrack, callback) => {
-    const [subscriber, setSubscriber] = useState(null);
     const [err, setErr] = useState(null);
 
-    const startWatching = async () => {
-        try {
-            const { granted } = await requestForegroundPermissionsAsync();
-            const sub = await watchPositionAsync(
-                {
-                    accuracy: Accuracy.BestForNavigation,
-                    timeInterval: 1000,
-                    distanceInterval: 10,
-                },
-                callback
-            );
-
-            setSubscriber(sub);
-
-            if (!granted) {
-                throw new Error("Location permission not granted");
-            }
-        } catch (e) {
-            setErr(e);
-        }
-    };
-
     useEffect(() => {
+        let subscriber;
+        const startWatching = async () => {
+            try {
+                const { granted } = await requestForegroundPermissionsAsync();
+                subscriber = await watchPositionAsync(
+                    {
+                        accuracy: Accuracy.BestForNavigation,
+                        timeInterval: 1000,
+                        distanceInterval: 10,
+                    },
+                    callback
+                );
+
+                if (!granted) {
+                    throw new Error("Location permission not granted");
+                }
+            } catch (e) {
+                setErr(e);
+            }
+        };
         if (shouldTrack) {
             startWatching();
         } else {
-            subscriber.remove();
-            setSubscriber(null);
+            if (subscriber) {
+                subscriber.remove();
+            }
+            subscriber = null;
         }
 
         return () => {
