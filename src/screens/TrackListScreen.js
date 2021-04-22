eb057d4e5d74c1cf484398e4123ba7c1d0b57236
Cory Bass <cory.bass.07@gmail.com>
2021-04-21 22:30:17 -0500

    gave trackdetail page the ability to filter by paramid

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -1,13 +1,41 @@
-import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet, Text, FlatList, TouchableOpacity } from "react-native";
+import { ListItem } from "react-native-elements";
+import { NavigationEvents } from "react-navigation";
+
+// context
+import { Context as TrackContext } from "../context/TrackContext";
 
 const TrackListScreen = ({ navigation }) => {
+    const { state, fetchTracks } = useContext(TrackContext);
+
+    console.log(state);
+
     return (
         <>
+            <NavigationEvents onWillFocus={fetchTracks} />
             <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
-            <Button
-                title="Go to Track Detail"
-                onPress={() => navigation.navigate("TrackDetail")}
+            <FlatList
+                data={state}
+                keyExtractor={(item) => item._id}
+                renderItem={({ item }) => {
+                    return (
+                        <TouchableOpacity
+                            onPress={() =>
+                                navigation.navigate("TrackDetail", {
+                                    _id: item._id,
+                                })
+                            }
+                        >
+                            <ListItem>
+                                <ListItem.Content>
+                                    <ListItem.Title>{item.name}</ListItem.Title>
+                                </ListItem.Content>
+                                <ListItem.Chevron />
+                            </ListItem>
+                        </TouchableOpacity>
+                    );
+                }}
             />
         </>
     );
